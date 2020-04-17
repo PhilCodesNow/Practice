e2fce9a3b018575f3fe5c96883eb00de1f4bd600
Philip Mayo <philipmayo@Philips-iMac.local>
2020-04-16 23:53:57 -0400

    rock paper scissors game started

---
+++ practice.js
@@ -0,0 +1,67 @@
+// let Arr = ['hello', 'hi', 'how are you?', 'good'];
+
+// for(let i = 0; i < Arr.length; i++){
+//     let newArr = Arr[i].split('');
+//     let upperCase = newArr[0].toUpperCase();
+//     newArr = newArr - newArr[0];
+//     upperCase = upperCase + newArr;
+//     console.log(upperCase);
+// }
+
+
+// object.values, object.keys, object.entries iterating over objects
+// const myArr = [1, 2, 3, 4, 5, 6];
+// const myObj = {name: 'alex', age: 34, byear: 1985};
+
+
+// myArr.forEach((value, index) => {
+//     console.log(index, value);
+// });
+
+
+// for(key in myObj){
+//     console.log(myObj[key]);
+// }
+
+
+
+// const objIt = Object.values(myObj);
+
+// for(let i = 0; i < objIt.length; i++){
+//     console.log(objIt[i]);
+// }
+
+// let objIt = Object.keys(myObj);
+
+// for(let i = 0; i < objIt.length; i++){
+//     console.log(myObj[objIt[i]]);
+// }
+
+
+
+// const myArr = [1, 2, 3, 4, 5, 6];
+// const myObj = {names:['alex', 'phil', 'john'], age: 34, byear: 1985}; 
+
+// Keys = Object.keys(myObj);
+
+
+
+// for(let i = 0; i < Keys.length; i++){
+//     if(myObj[Keys[i]] instanceof Array){
+//         for(let x = 0; x < Keys[i].length; i++){
+//             console.log(myObj[Keys[i][x]]);
+//         }
+//     }else{
+//         console.log(myObj[Keys[i]]);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
